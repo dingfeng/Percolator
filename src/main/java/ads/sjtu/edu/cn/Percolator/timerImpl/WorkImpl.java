de58cb32e9383f572acd41921e2d089e75af0561
dingfeng <1041460265@qq.com>
2018-03-31 14:55:08 +0800

    worker

--- src/main/java/ads/sjtu/edu/cn/Percolator/timerImpl/WorkImpl.java
+++ src/main/java/ads/sjtu/edu/cn/Percolator/timerImpl/WorkImpl.java
@@ -3,12 +3,23 @@ package ads.sjtu.edu.cn.Percolator.timerImpl;
 
 import ads.sjtu.edu.cn.Percolator.component.ThreadPool;
 import ads.sjtu.edu.cn.Percolator.timer.Worker;
+import ads.sjtu.edu.cn.Percolator.transaction.Conf;
+import ads.sjtu.edu.cn.Percolator.transaction.Transaction;
 import com.google.common.base.Throwables;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.filter.*;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -19,6 +30,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class WorkImpl implements Worker {
     static Logger logger = LoggerFactory.getLogger(WorkImpl.class);
+    private static final String RECORD_TABLE = "record_table";
+    private static final String ACCOUNT_TABLE = "account_table";
+    private static final int WORK_SIZE = 1000;
+    private static final float RANDOM_FILTER_CHANCE = 1.0f / WORK_SIZE;
+    private static final int RANDOM_PAGE_SIZE = 100;
     @Autowired
     private ThreadPool threadPool;
 
@@ -37,6 +53,34 @@ public class WorkImpl implements Worker {
 
         @Override
         public void run() {
+            try (Connection connection = ConnectionFactory.createConnection(Conf.HBASE_CONFIG)) {
+                HTable accountTable = (HTable) connection.getTable(TableName.valueOf(ACCOUNT_TABLE));
+                //filter by notification column
+                List<Filter> filters = new ArrayList<>();
+                //value filter
+                Filter singleColumnValueFilter = new SingleColumnValueExcludeFilter(Bytes.toBytes(Transaction.NOTIFICATION_FAMILY), Bytes.toBytes(Transaction.NOTIFICATION_FAMILY), CompareFilter.CompareOp.EQUAL, new byte[]{1});
+                filters.add(singleColumnValueFilter);
+                RandomRowFilter randomRowFilter = new RandomRowFilter(RANDOM_FILTER_CHANCE);
+                filters.add(randomRowFilter);
+                PageFilter pageFilter = new PageFilter(RANDOM_PAGE_SIZE);
+                filters.add(pageFilter);
+                //random filter
+                Filter filterList = new FilterList(filters);
+                Scan accountTableScan = new Scan();
+                accountTableScan.setFilter(filterList);
+                ResultScanner randomScanner = accountTable.getScanner(accountTableScan);
+                List<byte[]> randomRowList = new ArrayList<>();
+                try {
+                    for (Result randomScannerResult : randomScanner) {
+                        byte[] row = randomScannerResult.getRow();
+                        randomRowList.add(row);
+                    }
+                } finally {
+                    randomScanner.close();
+                }
+            } catch (IOException e) {
+                logger.error(Throwables.getStackTraceAsString(e));
+            }
 
         }
     }
--- src/main/java/ads/sjtu/edu/cn/Percolator/transaction/Transaction.java
+++ src/main/java/ads/sjtu/edu/cn/Percolator/transaction/Transaction.java
@@ -22,15 +22,15 @@ public class Transaction {
     public final static String LOCK_COl = "lock";
     public final static String WRITE_COL = "write";
     public final static String DATA_COL = "data";
-    public final static String NOTIFICATION = "notification";
-    private Connection connection;
+    public final static String NOTIFICATION_FAMILY = "notification";
+    public final static String NOTIFICATION_FAMILY_FLAG="flag";
     private long startTimestamp;
     private List<Write> writes = new ArrayList<>();
 
     public Transaction() throws IOException {
-        this.connection = ConnectionFactory.createConnection(Conf.HBASE_CONFIG);
         this.startTimestamp = getOneTimestamp();
     }
+
 
     private long getOneTimestamp() throws RemoteException {
         return SupportServiceClient.getInstance().getTimestamps(1).get(0);
@@ -76,7 +76,7 @@ public class Transaction {
             RowMutations rowMutations = new RowMutations(Bytes.toBytes(row));
             Delete lockDelete = new Delete(Bytes.toBytes(row)).addColumn(Bytes.toBytes(family), Bytes.toBytes(LOCK_COl)).setTimestamp(rowResultTimestamp);
             Put writePut = new Put(Bytes.toBytes(row)).addColumn(Bytes.toBytes(family), Bytes.toBytes(WRITE_COL), primaryCommitTimestamp, Bytes.toBytes(rowResultTimestamp));
-            Put notificationPut = new Put(Bytes.toBytes(row)).addColumn(Bytes.toBytes(family), Bytes.toBytes(NOTIFICATION), primaryCommitTimestamp, new byte[]{1});
+            Put notificationPut = new Put(Bytes.toBytes(row)).addColumn(Bytes.toBytes(NOTIFICATION_FAMILY), Bytes.toBytes(NOTIFICATION_FAMILY_FLAG), primaryCommitTimestamp, new byte[]{1});
             rowMutations.add(lockDelete);
             rowMutations.add(writePut);
             rowMutations.add(notificationPut);
@@ -86,37 +86,42 @@ public class Transaction {
     }
 
     public Long get(String row, String col) throws IOException {
-        HTable table = (HTable) this.connection.getTable(TableName.valueOf(TABLE_NAME));
-        byte[] rowBytes = Bytes.toBytes(row);
-        byte[] familyBytes = Bytes.toBytes(col);
-        while (true) {
-            Get lockGet = new Get(rowBytes);
-            lockGet.setTimeRange(0, startTimestamp);
-            lockGet.addColumn(familyBytes, Bytes.toBytes(LOCK_COl));
-            if (table.exists(lockGet)) {
-                logger.info("row = {} has been locked", row);
-                backoffAndMaybeCleanuplock(table, row, col);
-                continue;
+        try (Connection connection = ConnectionFactory.createConnection(Conf.HBASE_CONFIG);) {
+            HTable table = (HTable) connection.getTable(TableName.valueOf(TABLE_NAME));
+            byte[] rowBytes = Bytes.toBytes(row);
+            byte[] familyBytes = Bytes.toBytes(col);
+            while (true) {
+                Get lockGet = new Get(rowBytes);
+                lockGet.setTimeRange(0, startTimestamp);
+                lockGet.addColumn(familyBytes, Bytes.toBytes(LOCK_COl));
+                if (table.exists(lockGet)) {
+                    logger.info("row = {} has been locked", row);
+                    backoffAndMaybeCleanuplock(table, row, col);
+                    continue;
+                }
+                Result result = table.get(new Get(rowBytes).setTimeRange(0, startTimestamp).addColumn(familyBytes, Bytes.toBytes("write")));
+                if (result.rawCells() == null || result.rawCells().length == 0) {
+                    logger.warn("no result for rowKey={} ", row);
+                    return null;
+                }
+                long lastest_write = Bytes.toLong(result.getValue(familyBytes, Bytes.toBytes("write")));
+                Result dataResult = table.get(new Get(rowBytes).setTimeStamp(lastest_write).addColumn(familyBytes, Bytes.toBytes("data")));
+                byte[] dataBytes = dataResult.getValue(familyBytes, Bytes.toBytes("data"));
+                return Bytes.toLong(dataBytes);
             }
-            Result result = table.get(new Get(rowBytes).setTimeRange(0, startTimestamp).addColumn(familyBytes, Bytes.toBytes("write")));
-            if (result.rawCells() == null || result.rawCells().length == 0) {
-                logger.warn("no result for rowKey={} ", row);
-                return null;
-            }
-            long lastest_write = Bytes.toLong(result.getValue(familyBytes, Bytes.toBytes("write")));
-            Result dataResult = table.get(new Get(rowBytes).setTimeStamp(lastest_write).addColumn(familyBytes, Bytes.toBytes("data")));
-            byte[] dataBytes = dataResult.getValue(familyBytes, Bytes.toBytes("data"));
-            return Bytes.toLong(dataBytes);
+        } catch (IOException e) {
+            throw e;
         }
     }
 
 
-    public boolean pWrite(Write w, Write primary) throws IOException {
+    private boolean pWrite(Connection connection, Write w, Write primary) throws IOException {
+
         String row = w.getRow();
         String col = w.getCol();
         RowTransaction rowTransaction = new RowTransaction(TABLE_NAME, row);
         rowTransaction.startRowTransaction();
-        HTable table = (HTable) this.connection.getTable(TableName.valueOf(TABLE_NAME));
+        HTable table = (HTable) connection.getTable(TableName.valueOf(TABLE_NAME));
         boolean writeExist = table.exists(new Get(Bytes.toBytes(row)).addColumn(Bytes.toBytes(col), Bytes.toBytes(WRITE_COL)).setTimeRange(startTimestamp, Long.MAX_VALUE));
         if (writeExist) {
             return false;
@@ -138,35 +143,39 @@ public class Transaction {
             logger.info("commit empty writes");
             return true;
         }
-        Write primary = writes.get(0);
-        if (!pWrite(primary, primary)) return false;
-        for (int i = 1; i < writes.size(); ++i) {
-            Write wirte = writes.get(i);
-            if (!pWrite(wirte, primary)) return false;
-        }
-        long commitTimestamp = getOneTimestamp();
-        logger.info("primary start row transaction");
-        RowTransaction rowTransaction = new RowTransaction(TABLE_NAME, primary.getRow());
-        rowTransaction.startRowTransaction();
-        HTable table = (HTable) this.connection.getTable(TableName.valueOf(TABLE_NAME));
-        if (!table.exists(new Get(Bytes.toBytes(primary.getRow())).addColumn(Bytes.toBytes(primary.getCol()), Bytes.toBytes(LOCK_COl)).setTimeStamp(startTimestamp)))
-            return false;
-        RowMutations mutations = new RowMutations(Bytes.toBytes(primary.getRow()));
-        mutations.add(new Put(Bytes.toBytes(primary.getRow())).addColumn(Bytes.toBytes(primary.getCol()), Bytes.toBytes(WRITE_COL), commitTimestamp, Bytes.toBytes(startTimestamp)));
-        mutations.add(new Delete(Bytes.toBytes(primary.getRow())).addColumn(Bytes.toBytes(primary.getCol()), Bytes.toBytes(LOCK_COl)));
-        mutations.add(new Put(Bytes.toBytes(primary.getRow())).addColumn(Bytes.toBytes(primary.getCol()), Bytes.toBytes(NOTIFICATION), commitTimestamp, new byte[]{1}));
+        try (Connection connection = ConnectionFactory.createConnection(Conf.HBASE_CONFIG)) {
+            Write primary = writes.get(0);
+            if (!pWrite(connection, primary, primary)) return false;
+            for (int i = 1; i < writes.size(); ++i) {
+                Write wirte = writes.get(i);
+                if (!pWrite(connection, wirte, primary)) return false;
+            }
+            long commitTimestamp = getOneTimestamp();
+            logger.info("primary start row transaction");
+            RowTransaction rowTransaction = new RowTransaction(TABLE_NAME, primary.getRow());
+            rowTransaction.startRowTransaction();
+            HTable table = (HTable) connection.getTable(TableName.valueOf(TABLE_NAME));
+            if (!table.exists(new Get(Bytes.toBytes(primary.getRow())).addColumn(Bytes.toBytes(primary.getCol()), Bytes.toBytes(LOCK_COl)).setTimeStamp(startTimestamp)))
+                return false;
+            RowMutations mutations = new RowMutations(Bytes.toBytes(primary.getRow()));
+            mutations.add(new Put(Bytes.toBytes(primary.getRow())).addColumn(Bytes.toBytes(primary.getCol()), Bytes.toBytes(WRITE_COL), commitTimestamp, Bytes.toBytes(startTimestamp)));
+            mutations.add(new Delete(Bytes.toBytes(primary.getRow())).addColumn(Bytes.toBytes(primary.getCol()), Bytes.toBytes(LOCK_COl)));
+            mutations.add(new Put(Bytes.toBytes(primary.getRow())).addColumn(Bytes.toBytes(NOTIFICATION_FAMILY), Bytes.toBytes(NOTIFICATION_FAMILY_FLAG), commitTimestamp, new byte[]{1}));
 
-        table.mutateRow(mutations);
-        if (!rowTransaction.commit()) return false;
-        logger.info("primary succeeds to commit row transaction");
-        //update other rows
-        for (int i = 1; i < writes.size(); ++i) {
-            Write write = writes.get(i);
-            RowMutations rowMutations = new RowMutations(Bytes.toBytes(write.getRow()));
-            rowMutations.add(new Put(Bytes.toBytes(write.getRow())).addColumn(Bytes.toBytes(write.getCol()), Bytes.toBytes(WRITE_COL), commitTimestamp, Bytes.toBytes(startTimestamp)));
-            rowMutations.add(new Delete(Bytes.toBytes(write.getRow())).addColumn(Bytes.toBytes(write.getCol()), Bytes.toBytes(LOCK_COl)));
-            rowMutations.add(new Put(Bytes.toBytes(write.getRow())).addColumn(Bytes.toBytes(write.getCol()), Bytes.toBytes(NOTIFICATION), commitTimestamp, new byte[]{1}));
-            table.mutateRow(rowMutations);
+            table.mutateRow(mutations);
+            if (!rowTransaction.commit()) return false;
+            logger.info("primary succeeds to commit row transaction");
+            //update other rows
+            for (int i = 1; i < writes.size(); ++i) {
+                Write write = writes.get(i);
+                RowMutations rowMutations = new RowMutations(Bytes.toBytes(write.getRow()));
+                rowMutations.add(new Put(Bytes.toBytes(write.getRow())).addColumn(Bytes.toBytes(write.getCol()), Bytes.toBytes(WRITE_COL), commitTimestamp, Bytes.toBytes(startTimestamp)));
+                rowMutations.add(new Delete(Bytes.toBytes(write.getRow())).addColumn(Bytes.toBytes(write.getCol()), Bytes.toBytes(LOCK_COl)));
+                rowMutations.add(new Put(Bytes.toBytes(write.getRow())).addColumn(Bytes.toBytes(NOTIFICATION_FAMILY), Bytes.toBytes(NOTIFICATION_FAMILY_FLAG), commitTimestamp, new byte[]{1}));
+                table.mutateRow(rowMutations);
+            }
+        } catch (IOException e) {
+            throw e;
         }
         logger.info("transaction commited");
         return true;
