a76deb425fbe21505b8ad3aa983a24877256065b
dingfeng <1041460265@qq.com>
2018-04-01 17:51:55 +0800

    允许跨域访问

--- src/main/java/ads/sjtu/edu/cn/Percolator/PercolatorApplication.java
+++ src/main/java/ads/sjtu/edu/cn/Percolator/PercolatorApplication.java
@@ -2,7 +2,11 @@ package ads.sjtu.edu.cn.Percolator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableScheduling
@@ -10,5 +14,17 @@ public class PercolatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PercolatorApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedHeaders("*")
+						.allowedMethods("*")
+						.allowedOrigins("*");
+			}
+		};
 	}
 }
